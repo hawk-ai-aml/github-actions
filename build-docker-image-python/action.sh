c2a8f6f19ac773ea814c2a03f027bc86ba5e8d14
Thinh Tran <thinh.tran@hawk.ai>
2025-05-08 19:13:15 +0200

    some more improvements to the dockerfile

--- build-docker-image-python/action.sh
+++ build-docker-image-python/action.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+set -euo pipefail
 
 # Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No color
 
+export PYTHONUNBUFFERED=1
+export DOCKER_BUILDKIT=1
+
 # Build and push Docker image
 build_and_push() {
     local image_name=$1
     local build_args=$2
 
-    set -euo pipefail
-
     echo -e "${GREEN} Building Docker image: ${image_name}${NC}"
-    DOCKER_BUILDKIT=1 docker build --progress=plain $build_args -t "${image_name}:${IMAGE_TAG}" .
+    DOCKER_BUILDKIT=1 docker build --progress=plain $build_args -t "${image_name}:${IMAGE_TAG}" . | tee /dev/stdout
     docker push "${image_name}:${IMAGE_TAG}"
 }
 
